58e402db227b180341446d16885f2bdefa1577b7
AlexSerg24 <ars13@tpu.ru>
2021-05-19 13:55:51 +0700

    Не получается подключиться к серверу...

--- Assets/Scripts/TranlationMover.cs
+++ Assets/Scripts/TranlationMover.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using UnityEngine;
 using PathCreation;
+using System.Data;
+using System.Data.SqlClient;
 
 public class TranlationMover : MonoBehaviour
 {
@@ -23,11 +25,17 @@ public class TranlationMover : MonoBehaviour
     private bool shouldMove = true;
     private bool hasTurned = false;
 
+    //private string connectionString;
+    SqlCommand sqlCommand;
+    SqlConnection sqlConnection;
+    private string result;
+
     // Start is called before the first frame update
     void Start()
     {
         Points[0] = turnPoint.transform;
         Points[1] = straightEndPoint.transform;
+
     }
 
     void Update()
@@ -49,9 +57,19 @@ public class TranlationMover : MonoBehaviour
                 transform.Translate(Vector3.Normalize(Points[targetPointID].position - transform.position) * Time.deltaTime * speed);
                 if (targetDistance < 0.5f)
                 {
-                    if ((targetPointID == 0) && (raillsRotation.railsTurn))
+                    if (targetPointID == 0)
                     {
-                        hasTurned = true;
+                        if (raillsRotation.railsTurn)
+                        {
+                            hasTurned = true;
+                            result = "turn";
+                            SaveResult(result);
+                        }
+                        else
+                        {
+                            result = "not turn";
+                            SaveResult(result);
+                        }
                     }
                     if (targetPointID < Points.Length - 1)
                     {
@@ -117,4 +135,54 @@ public class TranlationMover : MonoBehaviour
         Quaternion rotationY = Quaternion.AngleAxis(1, Vector3.up);
         transform.rotation *= rotationY;
     }*/
+
+    private void SaveResult(string result)
+    {
+        
+       
+            /*SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "appserver01.main.tpu.ru\\sqlvt";
+            builder.UserID = "TPU\\ars13";
+            builder.Password = "MC03LkA3";
+            builder.InitialCatalog = "trolley_problem";
+            builder.IntegratedSecurity = true;
+
+            SqlConnection connection = new SqlConnection(builder.ConnectionString);*/
+
+            Debug.Log("Connection...");
+            //connectionString = @"Data Source = .;user id = .;password = .; "
+            sqlConnection = new SqlConnection("Data Source=appserver01.main.tpu.ru;User ID=ars13;Password=MC03LkA3;Initial Catalog=trolley_problem;Integrated Security=true"); //(SQL Server 14.0.3356.20 - TPU\ars13)
+            string userId = "1337";
+            //bool isFirst = true;
+            int isFirst = 0;
+            sqlConnection.Open();
+            //string connStr = "server=localhost;user=root;database=selection_problems;password=0000;"; // строка подключения к БД
+            //MySqlConnection connection = new MySqlConnection(connStr); // создаём объект для подключения к БД
+            //connection.Open(); // устанавливаем соединение с БД
+
+            string query = string.Format("SELECT result FROM results WHERE user_id='{0}'", userId);
+            sqlCommand = new SqlCommand(query, sqlConnection);
+
+            
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+
+            //isFirst = !reader.HasRows;
+            if (reader.HasRows)
+            {
+                isFirst = 0;
+            }
+            else
+            {
+                isFirst = 1;
+            }
+            reader.Close();
+
+
+            query = string.Format("INSERT INTO results (user_id, result, is_first) VALUES ({0}, {1}, {2})", userId, result, isFirst);
+            sqlCommand = new SqlCommand(query, sqlConnection);
+
+            sqlCommand.ExecuteNonQuery();
+            sqlConnection.Close(); // закрываем соединение с БД
+
+    }
 }
