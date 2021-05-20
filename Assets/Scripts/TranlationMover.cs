f15a7310d8784d71443f4e5665a265f76411d310
AlexSerg24 <ars13@tpu.ru>
2021-05-20 16:31:36 +0700

    Недоработанное меню

--- Assets/Scripts/TranlationMover.cs
+++ Assets/Scripts/TranlationMover.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using UnityEngine;
 using PathCreation;
+//using MySql.Data;
+//using MySql.Data.MySqlClient;
 
 public class TranlationMover : MonoBehaviour
 {
@@ -49,9 +51,17 @@ public class TranlationMover : MonoBehaviour
                 transform.Translate(Vector3.Normalize(Points[targetPointID].position - transform.position) * Time.deltaTime * speed);
                 if (targetDistance < 0.5f)
                 {
-                    if ((targetPointID == 0) && (raillsRotation.railsTurn))
+                    if (targetPointID == 0)
                     {
-                        hasTurned = true;
+                        if (raillsRotation.railsTurn)
+                        {
+                            hasTurned = true;
+                            // SaveResult("Повернул стрелку");
+                        }
+                        else
+                        {
+                            // SaveResult("Не повернул стрелку");
+                        }
                     }
                     if (targetPointID < Points.Length - 1)
                     {
@@ -116,5 +126,28 @@ public class TranlationMover : MonoBehaviour
     {
         Quaternion rotationY = Quaternion.AngleAxis(1, Vector3.up);
         transform.rotation *= rotationY;
+    }*/
+
+
+    // НУЖНАЯ ФУНКЦИЯ
+    /*private void SaveResult(string result)
+    {
+        string userId = "1337";
+        bool isFirst = true;
+
+        string connStr = "server=localhost;user=root;database=selection_problems;password=0000;"; // строка подключения к БД
+        MySqlConnection connection = new MySqlConnection(connStr); // создаём объект для подключения к БД
+        connection.Open(); // устанавливаем соединение с БД
+
+        string query = string.Format("SELECT result FROM trolley_problem WHERE user_id='{0}'", userId);
+        MySqlCommand command = new MySqlCommand(query, connection);
+        MySqlDataReader reader = command.ExecuteReader();
+        isFirst = !reader.HasRows;
+
+        query = string.Format("INSERT INTO trolley_problem (user_id, result, is_first) VALUES ({0}, {1}, {2})", userId, result, isFirst);
+        command = new MySqlCommand(query, connection);
+        command.ExecuteNonQuery();
+
+        connection.Close(); // закрываем соединение с БД
     }*/
 }
