7ad6ec10306273f6161e16e61132e7a1ae1f84ae
AlexSerg24 <ars13@tpu.ru>
2021-06-02 20:06:15 +0700

    Запись в файл работает, также внесли немного правок

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,11 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.IO;
 
 public class Menu : MonoBehaviour
 {
     public GameObject buttonsMenu;
     public GameObject buttonsExit;
+    public GameObject buttonsChoose;
+    public GameObject pannel;
+
+    public GameObject Zenaza;
+    public GameObject Qiana;
+    public GameObject Disoni;
+    public GameObject Powan;
+    public GameObject Qiaoaa;
+    public GameObject Nubina;
+    public GameObject Colma;
+    public GameObject Ignar;
+    public GameObject Darrill;
+    public GameObject Jordell;
+
+    private string nickname;
+
+    private string playerDataPath = Application.streamingAssetsPath + "/Results/" + "result" + ".db";
 
     // Start is called before the first frame update
     void Start()
@@ -23,12 +43,24 @@ public class Menu : MonoBehaviour
     {
         buttonsMenu.SetActive (false);
         buttonsExit.SetActive (true);
+        buttonsChoose.SetActive(false);
+        pannel.SetActive(true);
     }
 
     public void BackInMenu()
     {
         buttonsMenu.SetActive(true);
         buttonsExit.SetActive(false);
+        buttonsChoose.SetActive(false);
+        pannel.SetActive(true);
+    }
+
+    public void ChooseName()
+    {
+        buttonsMenu.SetActive(false);
+        buttonsExit.SetActive(false);
+        buttonsChoose.SetActive(true);
+        pannel.SetActive(false);
     }
 
     public void ExitGame()
@@ -38,6 +70,326 @@ public class Menu : MonoBehaviour
 
     public void NewGameLoadSceneSimpleSxene()
     {
+        Application.LoadLevel("SampleScene");
+    }
+
+    public void NewGameZenaza()
+    {
+        nickname = Zenaza.GetComponent<Text>().text;
+        try
+        {
+            if (!File.Exists(playerDataPath))
+            {
+                using (StreamWriter sw = new StreamWriter(playerDataPath, false, System.Text.Encoding.Default))
+                {
+                    sw.Write(nickname + "&");
+                    sw.Flush();
+                    sw.Close();
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = new StreamWriter(playerDataPath, true, System.Text.Encoding.Default))
+                {
+                    sw.Write(nickname + "&");
+                    sw.Flush();
+                    sw.Close();
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+
+        Application.LoadLevel("SampleScene");
+    }
+
+    public void NewGameQiana()
+    {
+        nickname = Qiana.GetComponent<Text>().text;
+        try
+        {
+            if (!File.Exists(playerDataPath))
+            {
+                using (StreamWriter sw = new StreamWriter(playerDataPath, false, System.Text.Encoding.Default))
+                {
+                    sw.Write(nickname + "&");
+                    sw.Flush();
+                    sw.Close();
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = new StreamWriter(playerDataPath, true, System.Text.Encoding.Default))
+                {
+                    sw.Write(nickname + "&");
+                    sw.Flush();
+                    sw.Close();
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+
+        Application.LoadLevel("SampleScene");
+    }
+
+    public void NewGameDisoni()
+    {
+        nickname = Disoni.GetComponent<Text>().text;
+        try
+        {
+            if (!File.Exists(playerDataPath))
+            {
+                using (StreamWriter sw = new StreamWriter(playerDataPath, false, System.Text.Encoding.Default))
+                {
+                    sw.Write(nickname + "&");
+                    sw.Flush();
+                    sw.Close();
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = new StreamWriter(playerDataPath, true, System.Text.Encoding.Default))
+                {
+                    sw.Write(nickname + "&");
+                    sw.Flush();
+                    sw.Close();
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+
+        Application.LoadLevel("SampleScene");
+    }
+
+    public void NewGamePowan()
+    {
+        nickname = Powan.GetComponent<Text>().text;
+        try
+        {
+            if (!File.Exists(playerDataPath))
+            {
+                using (StreamWriter sw = new StreamWriter(playerDataPath, false, System.Text.Encoding.Default))
+                {
+                    sw.Write(nickname + "&");
+                    sw.Flush();
+                    sw.Close();
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = new StreamWriter(playerDataPath, true, System.Text.Encoding.Default))
+                {
+                    sw.Write(nickname + "&");
+                    sw.Flush();
+                    sw.Close();
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+
+        Application.LoadLevel("SampleScene");
+    }
+
+    public void NewGameQiaoaa()
+    {
+        nickname = Qiaoaa.GetComponent<Text>().text;
+        try
+        {
+            if (!File.Exists(playerDataPath))
+            {
+                using (StreamWriter sw = new StreamWriter(playerDataPath, false, System.Text.Encoding.Default))
+                {
+                    sw.Write(nickname + "&");
+                    sw.Flush();
+                    sw.Close();
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = new StreamWriter(playerDataPath, true, System.Text.Encoding.Default))
+                {
+                    sw.Write(nickname + "&");
+                    sw.Flush();
+                    sw.Close();
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+
+        Application.LoadLevel("SampleScene");
+    }
+
+    public void NewGameNubina()
+    {
+        nickname = Nubina.GetComponent<Text>().text;
+        try
+        {
+            if (!File.Exists(playerDataPath))
+            {
+                using (StreamWriter sw = new StreamWriter(playerDataPath, false, System.Text.Encoding.Default))
+                {
+                    sw.Write(nickname + "&");
+                    sw.Flush();
+                    sw.Close();
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = new StreamWriter(playerDataPath, true, System.Text.Encoding.Default))
+                {
+                    sw.Write(nickname + "&");
+                    sw.Flush();
+                    sw.Close();
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+
+        Application.LoadLevel("SampleScene");
+    }
+
+    public void NewGameColma()
+    {
+        nickname = Colma.GetComponent<Text>().text;
+        try
+        {
+            if (!File.Exists(playerDataPath))
+            {
+                using (StreamWriter sw = new StreamWriter(playerDataPath, false, System.Text.Encoding.Default))
+                {
+                    sw.Write(nickname + "&");
+                    sw.Flush();
+                    sw.Close();
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = new StreamWriter(playerDataPath, true, System.Text.Encoding.Default))
+                {
+                    sw.Write(nickname + "&");
+                    sw.Flush();
+                    sw.Close();
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+
+        Application.LoadLevel("SampleScene");
+    }
+
+    public void NewGameIgnar()
+    {
+        nickname = Ignar.GetComponent<Text>().text;
+        try
+        {
+            if (!File.Exists(playerDataPath))
+            {
+                using (StreamWriter sw = new StreamWriter(playerDataPath, false, System.Text.Encoding.Default))
+                {
+                    sw.Write(nickname + "&");
+                    sw.Flush();
+                    sw.Close();
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = new StreamWriter(playerDataPath, true, System.Text.Encoding.Default))
+                {
+                    sw.Write(nickname + "&");
+                    sw.Flush();
+                    sw.Close();
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+
+        Application.LoadLevel("SampleScene");
+    }
+
+    public void NewGameDarrill()
+    {
+        nickname = Darrill.GetComponent<Text>().text;
+        try
+        {
+            if (!File.Exists(playerDataPath))
+            {
+                using (StreamWriter sw = new StreamWriter(playerDataPath, false, System.Text.Encoding.Default))
+                {
+                    sw.Write(nickname + "&");
+                    sw.Flush();
+                    sw.Close();
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = new StreamWriter(playerDataPath, true, System.Text.Encoding.Default))
+                {
+                    sw.Write(nickname + "&");
+                    sw.Flush();
+                    sw.Close();
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+
+        Application.LoadLevel("SampleScene");
+    }
+
+    public void NewGameJordell()
+    {
+        nickname = Jordell.GetComponent<Text>().text;
+        try
+        {
+            if (!File.Exists(playerDataPath))
+            {
+                using (StreamWriter sw = new StreamWriter(playerDataPath, false, System.Text.Encoding.Default))
+                {
+                    sw.Write(nickname + "&");
+                    sw.Flush();
+                    sw.Close();
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = new StreamWriter(playerDataPath, true, System.Text.Encoding.Default))
+                {
+                    sw.Write(nickname + "&");
+                    sw.Flush();
+                    sw.Close();
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+
         Application.LoadLevel("SampleScene");
     }
 }
--- Assets/Scripts/MenuTrigger.cs
+++ Assets/Scripts/MenuTrigger.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+using System.IO;
 
 public class MenuTrigger : MonoBehaviour
 {
@@ -9,6 +11,11 @@ public class MenuTrigger : MonoBehaviour
     public GameObject menu;
     public GameObject menuText;
     public GameObject playerData;
+
+    private string line;
+    private string lastLine;
+
+    private string playerDataPath = Application.streamingAssetsPath + "/Results/" + "result" + ".db";
 
     private void OnTriggerEnter(Collider other)
     {
@@ -18,10 +25,46 @@ public class MenuTrigger : MonoBehaviour
             if (pt.text == "turn")
             {
                 menuText.GetComponent<Text>().text = "Вы сделали выбор вмешаться и спасти пять жизней, пожертвовав одной. Но стоило ли оно того? \r\nНа этом эксперимент окончен, спасибо за участие. Чтобы вернуться в главное меню, переключите рычаг у вас за спиной.";
+
+                using (StreamReader sr = new StreamReader(playerDataPath, System.Text.Encoding.Default))
+                {
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        lastLine = line;
+                    }
+                }
+
+                if (lastLine[lastLine.Length-1] == '&')
+                {
+                    using (StreamWriter sw = new StreamWriter(playerDataPath, true, System.Text.Encoding.Default))
+                    {
+                        sw.WriteLine("turn");
+                        sw.Flush();
+                        sw.Close();
+                    }
+                }
             }
             else if (pt.text == "not turn")
             {
                 menuText.GetComponent<Text>().text = "Вы сделали выбор не вмешиваться в чужие судьбы и пятеро человек всё-таки погибли. Но это же вас не касается, ведь так? \r\nНа этом эксперимент окончен, спасибо за участие. Чтобы вернуться в главное меню, переключите рычаг у вас за спиной.";
+
+                using (StreamReader sr = new StreamReader(playerDataPath, System.Text.Encoding.Default))
+                {
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        lastLine = line;
+                    }
+                }
+
+                if (lastLine[lastLine.Length - 1] == '&')
+                {
+                    using (StreamWriter sw = new StreamWriter(playerDataPath, true, System.Text.Encoding.Default))
+                    {
+                        sw.WriteLine("not turn");
+                        sw.Flush();
+                        sw.Close();
+                    }
+                }
             }
             else
             {
